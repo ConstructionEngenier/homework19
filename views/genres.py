5549ec972b440b498a4ef8798c00bbf5eaf8ab27
Artem <artem.mikhaylov5593@yandex.ru>
2022-04-03 20:55:21 +0300

    updated genres view

--- views/genres.py
+++ views/genres.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.genre import GenreSchema
@@ -13,6 +14,12 @@ class GenresView(Resource):
         res = GenreSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_required
+    def post(self):
+        req_json = request.json
+        new_genre = genre_service.create(req_json)
+        return f"Созданный genre_id: {new_genre.id}", 201
+
 
 @genre_ns.route('/<int:rid>')
 class GenreView(Resource):
@@ -20,3 +27,18 @@ class GenreView(Resource):
         r = genre_service.get_one(rid)
         sm_d = GenreSchema().dump(r)
         return sm_d, 200
+
+    @admin_required
+    def put(self, uid: int):
+        req_json = request.json
+        if not req_json.get('id'):
+            req_json['id'] = uid
+        if genre_service.update(req_json):
+            return f"Обновленный id: {uid}", 201
+        return "not found", 404
+
+    @admin_required
+    def delete(self, uid: int):
+        if genre_service.delete(uid):
+            return "", 204
+        return "not found", 404
